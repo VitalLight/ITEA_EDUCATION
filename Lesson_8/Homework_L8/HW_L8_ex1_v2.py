35017b4847315c590cb961036015f68623657fac
Vital <vetalhome.vh@gmail.com>
2020-03-22 00:07:10 +0200

    modifi

---
+++ Lesson_8/Homework_L8/HW_L8_ex1_v2.py
@@ -0,0 +1,40 @@
+"""
+Корисувач вказує дві назви файлів (у файлах якась текстова інформація).
+Після цього вказує, що знаходить спільне в даних файлах: Речення, слова, словосполучення.
+Вивести на екран кількість спільних елементів, які запросив користувач.
+"""
+import re
+
+find_object = input("Вкажіть, що потрбіно спільного віднайти у двох файлах?   ")
+
+# функція рахує кількіть вказаних символіву файлі
+def find_(o, f):
+    a = len(re.findall(o, f))
+    print("Вказане слово --- %s --- згадується  %s разів у файлі" % (find_object, a))
+    return a
+
+print("\nПерший введениий файл")
+file1 = open('d:\\Python_ITed\Lesson_8\\Homework_L8\\packeg_for_working with file\\ex_2_.txt', 'r')
+test1 = file1.read()
+find_(find_object, test1)
+# print("Вказане слово --- %s --- згадується  %s разів у файлі " % (find_object))
+file1.close()
+
+print("\nДругий введениий файл")
+with open('d:\\Python_ITed\\Lesson_8\\Homework_L8\\packeg_for_working with file\\kind_wine.txt', 'r') as file2:
+    test2 = file2.read()
+find_(find_object, test2)
+
+
+def enter_way_file ():
+    ways = []
+    index = 0
+    while True:
+        way = input("Введіть шлях до Першого файлу:  ")
+        ways.append(way)
+        index += 1
+
+
+
+
+
