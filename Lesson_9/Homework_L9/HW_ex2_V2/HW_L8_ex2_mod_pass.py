7d742e6a63b601d34ce2417f535873c017e87414
Vital <vetalhome.vh@gmail.com>
2020-04-22 23:34:10 +0300

    modif_Homework_L9

--- Lesson_9/Homework_L9/HW_ex2_V2/HW_L8_ex2_mod_pass.py
+++ Lesson_9/Homework_L9/HW_ex2_V2/HW_L8_ex2_mod_pass.py
@@ -1,60 +1,73 @@
-""""
-Password storage. Модифікувати програму генерування паролів, яку ви робили на 6 уроці.
-Після запуску програми, запистати корисувача, що він хоче зробити: створити новий чи отримати пароль,
- який він вводив раніше. Всі паролі зберігати в якомусь файлі. Отримати пароль можна за допомогою ключвого слова,
- яке вказує користувач при  створенні нового пароля.
+"""""
+Змінити логіку зберігання паролів у файлі у завданні з попереднього домашнього завдання.
+Зберігати дані у форматі json.
 
-Наприклад: Користувач запускає програму і хоче отримати пароль, який вводив раніше.
-Для цьго він водить ключове слово за яким введеться пошщук у файлі і отримується пароль.
-Коли корисувач хоче створити новий пароль, то використати код програми з 6 уроку, після генерувавння пароля попровисти
-корисувача ввести ключове слово, і дану пару ключ-значення записати у файл.
 """
 
-from Lesson_8.Homework_L8.to_password_packedg.passw import shufr
+import re
 
+from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.passw_L8_ex3 import shufr
+from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.unpassw_L8_ex3 import unshufr
 
-path_to_file_with_parol = "D:\\Python_ITed\\Lesson_8\\Homework_L8\\packeg_for_working with file\\passw_key.txt"
-e_ = input(" \nДля отримання доступу в систему оберіть:\n\t 1 - створити НОВИЙ пароль \n"
-           "\t2 - використати ЗБЕРЕЖЕНИЙ   \n")
+path_to_f = "D:\\Python_ITed\\Lesson_9\\Homework_L9\\HW_ex2_V2\\paswword.json"
+folder_with_f = 'D:\\Python_ITed\\Lesson_9\\Homework_L9\\HW_ex2_V2\\'
 
-if e_ != "1":
-    key = input("ВВЕДДІТЬ СВІЙ КЛЮЧ доступу до паролю   ")
-    l_key = len(key)
-    with open(path_to_file_with_parol, 'r') as f:
-        text = f.readlines()  # Створює масив елементом якого є рядок
-        passw = 0
-        for i in range(len(text)):
-            if key in text[i]:
-                g = text[i]
-                l_g = len(g)
-                passw = g[(l_key+3):]  # зріз рядка
-                if key not in text[i]:
-                    print("\nКлюч не знайдено. Cтворіть свій пароль та ключ")
-                    exit()
-        print("\nВаш пароль = %s" % passw)
+#  початок роботи- відкрити файл
+name_file = input("Вкажіть назву файлу, що знаходиться в \n" + folder_with_f + " без розширення  .txt ")
+with open(folder_with_f + name_file + '.txt', 'r+') as f:
+    text = f.read()
 
-elif e_ == "1":
-    n_key, n_parol = shufr()
-    print("ВАШ ПАРОЛЬ ---  %s --- ТА КЛЮЧ  --- %s --- " %(n_parol, n_key))
-    with open(path_to_file_with_parol, 'a+') as f:
-        text = f.readlines()  # Створює масив елементом якого є рядок
-        f.write("\n"+n_key + " - " + n_parol)
+while True:
+    print(f"\nЩО РОБИТИ З ФАЙЛОМ {name_file}?")
+    answ = input("\nПОКАЗАТИ --- 1---, \nЗАШИФРУВАТИ(СТВОРИТИ НОВИЙ ПАРОЛЬ) --- 2---, \nРОЗШИФРУВАТИ --- 3---,"
+                 "\nВИХІД З ПРОГРАМИ, ЩОБ ОБРАТИ ІНШИЙ ФАЙЛ ДЛЯ РОБОТИ ---БУДЬ-ЯКИЙ ІНШИЙ СИМВОЛ   ")
+    if answ == "1":
+        print(text)
 
+    elif answ == "2":
+        #  запуск функції шифрування
+        key, shufr_text = shufr(text)
 
-# для L8_ex_3
-def find_key():
-    key = input("ВВЕДДІТЬ СВІЙ КЛЮЧ доступу до паролю   ")
-    l_key = len(key)
-    with open(r'Homework_L8\\to_password_packedg\\hw_L8_ex_3\\password_to_file.txt', 'r') as f:
-        text = f.readlines()  # створює масив елементом якого є рядок
-    passw = 0
-    for i in range(len(text)):
-        if key in text[i]:
-            g = text[i]
-            l_g = len(g)
-            passw = g[(l_key + 3):]  # зріз рядка вказано початкові координати
-            print("\nВаш пароль = %s" % passw)
-        elif key not in text[i]:
-            print("\nКлюч не знайдено. Cтворіть свій пароль та ключ")
-            exit()
-    return key
+        #  збереження зашифрованої інформації у новий файл
+        name_sekr_file = input("Вкажіть назву файлу для зберігання зашифров. інформації   ")
+        new_w_f = open(folder_with_f + name_sekr_file + '.txt', 'a+')
+        new_w_f.write(shufr_text)
+        new_w_f.close()
+
+        # збереження ключа у файл
+        key_file = open(path_to_f, 'a+')
+        dostup = key + " - " + name_sekr_file
+        key_file.write("\n" + dostup)  # перенесе запис на іншу стрічку
+        key_file.close()
+
+    elif answ == "3":
+        #  відкриття файлу для читання та перевірки чи є КЛЮЧ в файлі збереженими ключами
+        key_f = open(path_to_f, 'r')
+        find_key_to_file = key_f.read()
+
+        key = input("Вкажіть КЛЮЧ   ")
+        #  визначення наявності пари КЛЮЧ-НАЗВА ФАЙЛУ
+        find_key_name_file = bool(''.join(re.findall(key + " - " + name_file, find_key_to_file)))
+
+        if find_key_name_file:
+            #  викликається функція з модуля по розшифруванню
+            unshufr_text, key = unshufr(text, key)
+
+            #  збереження розашифрованої інформації у файл
+            name_unsekr_file = input("Вкажіть назву файлу для зберігання розшифров. інформації   ")
+            new_w_f = open(r'D:\\Python_ITed\\Lesson_9\\Homework_L9\\HW_ex2_V2\\'
+                           + name_unsekr_file + '.txt', 'a+')
+            new_text = new_w_f.write(unshufr_text)
+
+            #  ЗАПИТАТИ ЧИ ПОТРІБНО СТВОРИТИ НОВИЙ ПАРОЛЬ ДЛЯ ЦЬОГО ФАЙЛУ
+            print("\n СТВОРИТИ НОВИЙ ПАРОЛЬ ДЛЯ ЦЬОГО ФАЙЛУ?")
+            ans2 = input("\nТАК --- 1,\n НІ ---2     ")
+            if ans2 == "1":
+                print("\nПЕРЕЗАПУСТІТЬ ПРОГРАМУ ТА ВИБЕРІТЬ ПОЗИЦІЮ ---2---")
+            elif ans2 == "2":
+                pass
+            new_w_f.close()
+        else:
+            print("Вказаний вами КЛЮЧ до цього файлу відсутній у базі даних.\n Файл розшифрувати НЕМОЖЛИВО")
+    else:
+        exit()
