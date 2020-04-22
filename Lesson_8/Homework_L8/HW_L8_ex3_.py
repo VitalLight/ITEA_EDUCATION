965ed62473426a726a5f0eb57504ee26d69fdbdb
Vital <vetalhome.vh@gmail.com>
2020-04-22 22:04:27 +0300

    modif_HW_L9_ex2

--- Lesson_8/Homework_L8/HW_L8_ex3_.py
+++ Lesson_8/Homework_L8/HW_L8_ex3_.py
@@ -7,6 +7,11 @@
 """
 import re
 
+from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.passw_L8_ex3 import shufr
+from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.unpassw_L8_ex3 import unshufr
+
+
+path_to_f = "d:\\Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3\\password_to_file.txt"
 #  початок роботи- відкрити файл
 name_file = input("Вкажіть назву файлу, що знаходиться в \n"
                   "Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3   без розширення  .txt ")
@@ -15,35 +20,32 @@ text = f.read()
 f.close()
 
 while True:
-    print("\nЩО РОБИТИ З ФАЙЛОМ ?")
+    print(f"\nЩО РОБИТИ З ФАЙЛОМ {name_file}?")
     answ = input("\nПОКАЗАТИ --- 1---, \nЗАШИФРУВАТИ --- 2---, \nРОЗШИФРУВАТИ --- 3---,\nВИХІД З ПРОГРАМИ "
                  "ЩОБ ОБРАТИ ІНШИЙ ФАЙЛ ДЛЯ РОБОТИ ---БУДЬ-ЯКИЙ ІНШИЙ СИМВОЛ   ")
     if answ == "1":
         print(text)
 
     elif answ == "2":
-            #  запуск функції шифрування
-        from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.passw_L8_ex3 import shufr
+        #  запуск функції шифрування
         key, shufr_text = shufr(text)
 
-            #  збереження зашифрованої інформації у новий файл
+        #  збереження зашифрованої інформації у новий файл
         name_sekr_file = input("Вкажіть назву файлу для зберігання зашифров. інформації   ")
         new_w_f = open(r'd:\\Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3\\'
-                       + name_sekr_file + '.txt','a+')
+                       + name_sekr_file + '.txt', 'a+')
         new_w_f.write(shufr_text)
         new_w_f.close()
 
         # збереження ключа у файл
-        key_file = open(r'd:\\Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3\\'
-                             r'password_to_file.txt','a+')
-        dostup = key + " - " + name_sekr_file  #  input("Введіть ")
-        key_file.write("\n"+ dostup)  # перенесе запис на іншу стрічку
+        key_file = open(path_to_f, 'a+')
+        dostup = key + " - " + name_sekr_file
+        key_file.write("\n" + dostup)  # перенесе запис на іншу стрічку
         key_file.close()
 
     elif answ == "3":
         #  відкриття файлу для читання та перевірки чи є КЛЮЧ в файлі збереженими ключами
-        key_f = open(r'd:\\Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3\\'
-                          r'password_to_file.txt', 'r')
+        key_f = open(path_to_f, 'r')
         find_key_to_file = key_f.read()
 
         key = input("Вкажіть КЛЮЧ   ")
@@ -52,11 +54,10 @@ while True:
 
         if find_key_name_file:
             #  викликається функція з модуля по розшифруванню
-            from Lesson_8.Homework_L8.to_password_packedg.hw_L8_ex_3.unpassw_L8_ex3 import unshufr
             unshufr_text, key = unshufr(text, key)
 
-            #  збереження зашифрованої інформації у файл
-            name_unsekr_file = input("Вкажіть назву файлу для зберігання зашифров. інформації   ")
+            #  збереження розашифрованої інформації у файл
+            name_unsekr_file = input("Вкажіть назву файлу для зберігання розашифров. інформації   ")
             new_w_f = open(r'd:\\Python_ITed\\Lesson_8\\Homework_L8\\to_password_packedg\\hw_L8_ex_3\\'
                            + name_unsekr_file + '.txt', 'a+')
             new_text = new_w_f.write(unshufr_text)
@@ -66,7 +67,7 @@ while True:
             ans2 = input("\nТАК --- 1,\n НІ ---2     ")
             if ans2 == "1":
                 print("\nПЕРЕЗАПУСТІТЬ ПРОГРАМУ ТА ВИБЕРІТЬ ПОЗИЦІЮ ---2---")
-            if ans2 == "2":
+            elif ans2 == "2":
                 pass
             new_w_f.close()
         else:
